8514686fb42c0a7a1cb069aec89d3860a2d889e2
Vinicius Augusto <vinaut.cr@gmail.com>
2025-01-07 17:20:19 -0300

    basic models

---
+++ app/Models/Plan.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Plan extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fiallable = [
+        'name',
+        'short_description',
+        'price'
+    ];
+}
